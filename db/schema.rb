43ac1d574f5020dc39904c06f02931129fec0cf7
michel <michel.szinavel@gmail.com>
2017-04-10 16:04:32 -0300

    Adding some files and folders to build Api structure, also added some packages methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410172409) do
+ActiveRecord::Schema.define(version: 20170410185130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170410172409) do
     t.json     "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "package_id"
   end
 
   create_table "statuses", force: :cascade do |t|
