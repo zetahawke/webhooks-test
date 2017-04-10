2d748b19a436a372d75bd4df565f4b4c379cfaea
Kyren <dev.esteban@gmail.com>
2017-04-10 15:33:39 -0300

    fixes

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,9 +1,10 @@
-class PackagesController < ApplicationController
+class PackagesController < ActionController::API
   def index
     @packages = Package.all
   end
 
-  def from_webhook
-    Package.create(data: params[:info])
+  def from_webhooks
+    binding.pry
+    Package.create(data: params[:info]) if Package.find_by(reference: params[:info][:reference])
   end
 end
