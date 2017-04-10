4d2dcde9efac3f5007db94ac23807c65cf47c54a
Kyren <dev.esteban@gmail.com>
2017-04-10 16:21:30 -0300

    fix info to body

--- app/controllers/api/packages_controller.rb
+++ app/controllers/api/packages_controller.rb
@@ -2,12 +2,11 @@ class Api::PackagesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def from_webhooks
-    binding.pry
-    @package = Package.find_by(package_id: params[:info][:id])
+    @package = Package.find_by(package_id: params[:body][:id])
     if @package.blank?
-      @package = Package.create(package_id: params[:info][:id], data: params[:info])
+      @package = Package.create(package_id: params[:body][:id], data: params[:body])
     else
-      @package = Package.update(data: params[:info])
+      @package = Package.update(data: params[:body])
     end
     render json: { package: @package }, status: :ok
   end
